2c13ad77b6cfd5e3bde579c25e9c2734ab2cf465
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2020-11-26 11:32:59 +0900

    make it possible to adjust canvas/room size

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { ChangeEvent, useCallback, useState } from "react";
 import { RecordList } from "./interfaces";
 import {
   DebounceFunc,
@@ -13,15 +13,77 @@ import { Room } from "./room";
 import { Sensor } from "./sensor";
 import { Simulator } from "./simulator";
 
+const useCanvas = (initW: number, initH: number) => {
+  const [canvasSize, setCanvasSize] = useState([initW, initH]);
+  const setCanvasWidth = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      setCanvasSize((prev) => [+target.value, prev[1]]),
+    [setCanvasSize]
+  );
+  const setCanvasHeight = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      setCanvasSize((prev) => [prev[0], +target.value]),
+    [setCanvasSize]
+  );
+  return [canvasSize, setCanvasWidth, setCanvasHeight] as const;
+};
+const useRoom = (
+  initL: number,
+  initT: number,
+  initW: number,
+  initH: number
+) => {
+  const [roomPosSize, serRoomPosSize] = useState([initL, initT, initW, initH]);
+  const setRoomLeft = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      serRoomPosSize((prev) => [+target.value, prev[1], prev[2], prev[3]]),
+    [serRoomPosSize]
+  );
+  const setRoomTop = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      serRoomPosSize((prev) => [prev[0], +target.value, prev[2], prev[3]]),
+    [serRoomPosSize]
+  );
+  const setRoomWidth = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      serRoomPosSize((prev) => [prev[0], prev[1], +target.value, prev[3]]),
+    [serRoomPosSize]
+  );
+  const setRoomHeight = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      serRoomPosSize((prev) => [prev[0], prev[1], prev[2], +target.value]),
+    [serRoomPosSize]
+  );
+  return [
+    roomPosSize,
+    setRoomLeft,
+    setRoomTop,
+    setRoomWidth,
+    setRoomHeight,
+  ] as const;
+};
+
 export const App = () => {
   const [recordLists, setRecordLists] = useState([] as RecordList[]);
   const addRecordList = useCallback(
     (recordList: RecordList) => setRecordLists((prev) => [...prev, recordList]),
     [setRecordLists]
   );
+  const [canvasSize, setCanvasWidth, setCanvasHeight] = useCanvas(500, 500);
+  const [
+    roomPosSize,
+    setRoomLeft,
+    setRoomTop,
+    setRoomWidth,
+    setRoomHeight,
+  ] = useRoom(100, 100, 300, 300);
+
   const room = new Room({
-    topLeft: { x: 100, y: 100 },
-    bottomRight: { x: 400, y: 400 },
+    topLeft: { x: roomPosSize[0], y: roomPosSize[1] },
+    bottomRight: {
+      x: roomPosSize[0] + roomPosSize[2],
+      y: roomPosSize[1] + roomPosSize[3],
+    },
     color: "gray",
   });
   const sensors = [
@@ -46,11 +108,65 @@ export const App = () => {
 
   return (
     <>
+      <p>
+        <label htmlFor="cvsW">Canvas width: </label>
+        <input
+          name="cvsW"
+          type="number"
+          value={canvasSize[0]}
+          onChange={setCanvasWidth}
+        />
+      </p>
+      <p>
+        <label htmlFor="cvsH">Canvas height: </label>
+        <input
+          name="cvsH"
+          type="number"
+          value={canvasSize[1]}
+          onChange={setCanvasHeight}
+        />
+      </p>
+      <p>
+        <label htmlFor="roomL">Room left: </label>
+        <input
+          name="roomL"
+          type="number"
+          value={roomPosSize[0]}
+          onChange={setRoomLeft}
+        />
+      </p>
+      <p>
+        <label htmlFor="roomH">Room top: </label>
+        <input
+          name="roomH"
+          type="number"
+          value={roomPosSize[1]}
+          onChange={setRoomTop}
+        />
+      </p>
+      <p>
+        <label htmlFor="roomW">Room width: </label>
+        <input
+          name="roomW"
+          type="number"
+          value={roomPosSize[2]}
+          onChange={setRoomWidth}
+        />
+      </p>
+      <p>
+        <label htmlFor="roomH">Room height: </label>
+        <input
+          name="roomH"
+          type="number"
+          value={roomPosSize[3]}
+          onChange={setRoomHeight}
+        />
+      </p>
       <section>
         <h1>Recorder</h1>
         <Recorder
-          width={500}
-          height={500}
+          width={canvasSize[0]}
+          height={canvasSize[1]}
           room={room}
           sensors={sensors}
           addRecordList={addRecordList}
@@ -60,8 +176,8 @@ export const App = () => {
       <section>
         <h1>Simulator</h1>
         <Simulator
-          width={500}
-          height={500}
+          width={canvasSize[0]}
+          height={canvasSize[1]}
           room={room}
           sensors={sensors}
           recordLists={recordLists}
