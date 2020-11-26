8d8150d5c035549f4c9ee6076cb5579917b1c6b9
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2020-11-26 12:30:51 +0900

    refacotring; split files

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,12 @@
-import React, { ChangeEvent, useCallback, useMemo, useState } from "react";
+import React, { useCallback, useState } from "react";
+import {
+  CanvasForm,
+  RoomForm,
+  SensorsForm,
+  useCanvas,
+  useRoom,
+  useSensors,
+} from "./forms";
 import { RecordList } from "./interfaces";
 import {
   DebounceFunc,
@@ -12,125 +20,6 @@ import { Recorder } from "./recorder";
 import { Room } from "./room";
 import { Sensor } from "./sensor";
 import { Simulator } from "./simulator";
-
-const useCanvas = (initW: number, initH: number) => {
-  const [canvasSize, setCanvasSize] = useState([initW, initH]);
-  const setCanvasWidth = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      setCanvasSize((prev) => [+target.value, prev[1]]),
-    [setCanvasSize]
-  );
-  const setCanvasHeight = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      setCanvasSize((prev) => [prev[0], +target.value]),
-    [setCanvasSize]
-  );
-  return [canvasSize, setCanvasWidth, setCanvasHeight] as const;
-};
-const useRoom = (
-  initL: number,
-  initT: number,
-  initW: number,
-  initH: number
-) => {
-  const [roomPosSize, serRoomPosSize] = useState([initL, initT, initW, initH]);
-  const setRoomLeft = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      serRoomPosSize((prev) => [+target.value, prev[1], prev[2], prev[3]]),
-    [serRoomPosSize]
-  );
-  const setRoomTop = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      serRoomPosSize((prev) => [prev[0], +target.value, prev[2], prev[3]]),
-    [serRoomPosSize]
-  );
-  const setRoomWidth = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      serRoomPosSize((prev) => [prev[0], prev[1], +target.value, prev[3]]),
-    [serRoomPosSize]
-  );
-  const setRoomHeight = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      serRoomPosSize((prev) => [prev[0], prev[1], prev[2], +target.value]),
-    [serRoomPosSize]
-  );
-  return [
-    roomPosSize,
-    setRoomLeft,
-    setRoomTop,
-    setRoomWidth,
-    setRoomHeight,
-  ] as const;
-};
-
-const useSensors = (initSensors: Sensor[]) => {
-  const [sensors, setSensors] = useState(initSensors);
-  const [attr, setAttr] = useState(["", 100, 100, 50] as [
-    string,
-    number,
-    number,
-    number
-  ]);
-  const isValidAttr = useMemo(
-    () =>
-      !!attr[0] &&
-      sensors.every((s) => s.id != attr[0]) &&
-      attr[1] > 0 &&
-      attr[2] > 0 &&
-      attr[3] > 0,
-    [attr, sensors]
-  );
-  const setSensorId = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      setAttr((prev) => [target.value, prev[1], prev[2], prev[3]]),
-    [setAttr]
-  );
-  const setSensorX = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      setAttr((prev) => [prev[0], +target.value, prev[2], prev[3]]),
-    [setAttr]
-  );
-  const setSensorY = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      setAttr((prev) => [prev[0], prev[1], +target.value, prev[3]]),
-    [setAttr]
-  );
-  const setSensorR = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      setAttr((prev) => [prev[0], prev[1], prev[2], +target.value]),
-    [setAttr]
-  );
-  const addSensor = useCallback(
-    () =>
-      setSensors((prev) => [
-        ...prev,
-        new Sensor({
-          id: attr[0],
-          x: attr[1],
-          y: attr[2],
-          r: attr[3],
-          color: "red",
-        }),
-      ]),
-    [attr, setSensors]
-  );
-  const removeSensor = useCallback(
-    (id: string) => setSensors((prev) => prev.filter((s) => s.id != id)),
-    [setSensors]
-  );
-
-  return [
-    sensors,
-    attr,
-    setSensorId,
-    setSensorX,
-    setSensorY,
-    setSensorR,
-    isValidAttr,
-    addSensor,
-    removeSensor,
-  ] as const;
-};
 
 export const App = () => {
   const [recordLists, setRecordLists] = useState([] as RecordList[]);
@@ -161,7 +50,6 @@ export const App = () => {
     new Sensor({ id: "s1", x: 325, y: 175, r: 80, color: "red" }),
     new Sensor({ id: "s2", x: 250, y: 325, r: 90, color: "red" }),
   ]);
-
   const room = new Room({
     topLeft: { x: roomPosSize[0], y: roomPosSize[1] },
     bottomRight: {
@@ -188,117 +76,35 @@ export const App = () => {
     <>
       <section>
         <h1>Configuration</h1>
-        <section>
-          <h2>Canvas</h2>
-          <p>
-            <label htmlFor="cvsW">width: </label>
-            <input
-              name="cvsW"
-              type="number"
-              value={canvasSize[0]}
-              onChange={setCanvasWidth}
-            />
-          </p>
-          <p>
-            <label htmlFor="cvsH">height: </label>
-            <input
-              name="cvsH"
-              type="number"
-              value={canvasSize[1]}
-              onChange={setCanvasHeight}
-            />
-          </p>
-        </section>
-        <section>
-          <h2>Room</h2>
-          <p>
-            <label htmlFor="roomL">left: </label>
-            <input
-              name="roomL"
-              type="number"
-              value={roomPosSize[0]}
-              onChange={setRoomLeft}
-            />
-          </p>
-          <p>
-            <label htmlFor="roomH">top: </label>
-            <input
-              name="roomH"
-              type="number"
-              value={roomPosSize[1]}
-              onChange={setRoomTop}
-            />
-          </p>
-          <p>
-            <label htmlFor="roomW">width: </label>
-            <input
-              name="roomW"
-              type="number"
-              value={roomPosSize[2]}
-              onChange={setRoomWidth}
-            />
-          </p>
-          <p>
-            <label htmlFor="roomH">height: </label>
-            <input
-              name="roomH"
-              type="number"
-              value={roomPosSize[3]}
-              onChange={setRoomHeight}
-            />
-          </p>
-        </section>
-        <section>
-          <h2>Sensors</h2>
-          <ul>
-            {sensors.map((s) => (
-              <li key={s.id}>
-                {s.id}{" "}
-                <input
-                  type="button"
-                  value="remove"
-                  onClick={() => removeSensor(s.id)}
-                />
-              </li>
-            ))}
-          </ul>
-          <p>
-            <label htmlFor="sensorId">id: </label>
-            <input
-              name="sensorId"
-              type="text"
-              value={sensorAttr[0]}
-              onChange={setSensorId}
-            />
-            <label htmlFor="sensorX">x: </label>
-            <input
-              name="sensorX"
-              type="number"
-              value={sensorAttr[1]}
-              onChange={setSensorX}
-            />
-            <label htmlFor="sensorY">y: </label>
-            <input
-              name="sensorY"
-              type="number"
-              value={sensorAttr[2]}
-              onChange={setSensorY}
-            />
-            <label htmlFor="sensorR">r: </label>
-            <input
-              name="sensorR"
-              type="number"
-              value={sensorAttr[3]}
-              onChange={setSensorR}
-            />
-            <input
-              type="button"
-              value="add"
-              disabled={!isValidAttr}
-              onClick={addSensor}
-            />
-          </p>
-        </section>
+        <CanvasForm
+          width={canvasSize[0]}
+          height={canvasSize[1]}
+          setWidth={setCanvasWidth}
+          setHeight={setCanvasHeight}
+        />
+        <RoomForm
+          left={roomPosSize[0]}
+          top={roomPosSize[1]}
+          width={roomPosSize[2]}
+          height={roomPosSize[3]}
+          setLeft={setRoomLeft}
+          setTop={setRoomTop}
+          setWidth={setRoomWidth}
+          setHeight={setRoomHeight}
+        />
+        <SensorsForm
+          sensors={sensors}
+          sensorAttr={sensorAttr}
+          setSensorId={setSensorId}
+          setSensorX={setSensorX}
+          setSensorY={setSensorY}
+          setSensorR={setSensorR}
+          isValidAttr={isValidAttr}
+          addSensor={addSensor}
+          removeSensor={removeSensor}
+          canvasWidth={canvasSize[0]}
+          canvasHeight={canvasSize[1]}
+        />
       </section>
       <section>
         <h1>Recorder</h1>
---
+++ src/forms.tsx
@@ -0,0 +1,280 @@
+import React, {
+  ChangeEvent,
+  FunctionComponent,
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
+import { Sensor } from "./sensor";
+
+export const useCanvas = (initW: number, initH: number) => {
+  const [canvasSize, setCanvasSize] = useState([initW, initH]);
+  const setCanvasWidth = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      setCanvasSize((prev) => [+target.value, prev[1]]),
+    [setCanvasSize]
+  );
+  const setCanvasHeight = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      setCanvasSize((prev) => [prev[0], +target.value]),
+    [setCanvasSize]
+  );
+  return [canvasSize, setCanvasWidth, setCanvasHeight] as const;
+};
+
+export const useRoom = (
+  initL: number,
+  initT: number,
+  initW: number,
+  initH: number
+) => {
+  const [roomPosSize, serRoomPosSize] = useState([initL, initT, initW, initH]);
+  const setRoomLeft = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      serRoomPosSize((prev) => [+target.value, prev[1], prev[2], prev[3]]),
+    [serRoomPosSize]
+  );
+  const setRoomTop = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      serRoomPosSize((prev) => [prev[0], +target.value, prev[2], prev[3]]),
+    [serRoomPosSize]
+  );
+  const setRoomWidth = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      serRoomPosSize((prev) => [prev[0], prev[1], +target.value, prev[3]]),
+    [serRoomPosSize]
+  );
+  const setRoomHeight = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      serRoomPosSize((prev) => [prev[0], prev[1], prev[2], +target.value]),
+    [serRoomPosSize]
+  );
+  return [
+    roomPosSize,
+    setRoomLeft,
+    setRoomTop,
+    setRoomWidth,
+    setRoomHeight,
+  ] as const;
+};
+
+export const useSensors = (initSensors: Sensor[]) => {
+  const [sensors, setSensors] = useState(initSensors);
+  const [attr, setAttr] = useState(["", 100, 100, 50] as [
+    string,
+    number,
+    number,
+    number
+  ]);
+  const isValidAttr = useMemo(
+    () =>
+      !!attr[0] &&
+      sensors.every((s) => s.id != attr[0]) &&
+      attr[1] > 0 &&
+      attr[2] > 0 &&
+      attr[3] > 0,
+    [attr, sensors]
+  );
+  const setSensorId = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      setAttr((prev) => [target.value, prev[1], prev[2], prev[3]]),
+    [setAttr]
+  );
+  const setSensorX = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      setAttr((prev) => [prev[0], +target.value, prev[2], prev[3]]),
+    [setAttr]
+  );
+  const setSensorY = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      setAttr((prev) => [prev[0], prev[1], +target.value, prev[3]]),
+    [setAttr]
+  );
+  const setSensorR = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      setAttr((prev) => [prev[0], prev[1], prev[2], +target.value]),
+    [setAttr]
+  );
+  const addSensor = useCallback(
+    () =>
+      setSensors((prev) => [
+        ...prev,
+        new Sensor({
+          id: attr[0],
+          x: attr[1],
+          y: attr[2],
+          r: attr[3],
+          color: "red",
+        }),
+      ]),
+    [attr, setSensors]
+  );
+  const removeSensor = useCallback(
+    (id: string) => setSensors((prev) => prev.filter((s) => s.id != id)),
+    [setSensors]
+  );
+
+  return [
+    sensors,
+    attr,
+    setSensorId,
+    setSensorX,
+    setSensorY,
+    setSensorR,
+    isValidAttr,
+    addSensor,
+    removeSensor,
+  ] as const;
+};
+
+interface CanvasFormProps {
+  width: number;
+  height: number;
+  setWidth(event: ChangeEvent<HTMLInputElement>): void;
+  setHeight(event: ChangeEvent<HTMLInputElement>): void;
+}
+export const CanvasForm: FunctionComponent<CanvasFormProps> = ({
+  width,
+  height,
+  setWidth,
+  setHeight,
+}) => (
+  <section>
+    <h2>Canvas</h2>
+    <p>
+      <label htmlFor="cvsW">width: </label>
+      <input name="cvsW" type="number" value={width} onChange={setWidth} />
+    </p>
+    <p>
+      <label htmlFor="cvsH">height: </label>
+      <input name="cvsH" type="number" value={height} onChange={setHeight} />
+    </p>
+  </section>
+);
+
+interface RoomFormProps {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+  setLeft(event: ChangeEvent<HTMLInputElement>): void;
+  setTop(event: ChangeEvent<HTMLInputElement>): void;
+  setWidth(event: ChangeEvent<HTMLInputElement>): void;
+  setHeight(event: ChangeEvent<HTMLInputElement>): void;
+}
+export const RoomForm: FunctionComponent<RoomFormProps> = ({
+  left,
+  top,
+  width,
+  height,
+  setLeft,
+  setTop,
+  setWidth,
+  setHeight,
+}) => (
+  <section>
+    <h2>Room</h2>
+    <p>
+      <label htmlFor="roomL">left: </label>
+      <input name="roomL" type="number" value={left} onChange={setLeft} />
+    </p>
+    <p>
+      <label htmlFor="roomT">top: </label>
+      <input name="roomT" type="number" value={top} onChange={setTop} />
+    </p>
+    <p>
+      <label htmlFor="roomW">width: </label>
+      <input name="roomW" type="number" value={width} onChange={setWidth} />
+    </p>
+    <p>
+      <label htmlFor="roomH">height: </label>
+      <input name="roomH" type="number" value={height} onChange={setHeight} />
+    </p>
+  </section>
+);
+
+interface SensorsFormProps {
+  sensors: Sensor[];
+  sensorAttr: [string, number, number, number];
+  setSensorId(event: ChangeEvent<HTMLInputElement>): void;
+  setSensorX(event: ChangeEvent<HTMLInputElement>): void;
+  setSensorY(event: ChangeEvent<HTMLInputElement>): void;
+  setSensorR(event: ChangeEvent<HTMLInputElement>): void;
+  isValidAttr: boolean;
+  addSensor(): void;
+  removeSensor(id: string): void;
+  canvasWidth: number;
+  canvasHeight: number;
+}
+export const SensorsForm: FunctionComponent<SensorsFormProps> = ({
+  sensors,
+  sensorAttr,
+  setSensorId,
+  setSensorX,
+  setSensorY,
+  setSensorR,
+  isValidAttr,
+  addSensor,
+  removeSensor,
+  canvasWidth,
+  canvasHeight,
+}) => (
+  <section>
+    <h2>Sensors</h2>
+    <ul>
+      {sensors.map((s) => (
+        <li key={s.id}>
+          {s.id}{" "}
+          <input
+            type="button"
+            value="remove"
+            onClick={() => removeSensor(s.id)}
+          />
+        </li>
+      ))}
+    </ul>
+    <p>
+      <label htmlFor="sensorId">id: </label>
+      <input
+        name="sensorId"
+        type="text"
+        maxLength={16}
+        value={sensorAttr[0]}
+        onChange={setSensorId}
+      />
+      <label htmlFor="sensorX">x: </label>
+      <input
+        name="sensorX"
+        type="number"
+        min={0}
+        max={canvasWidth}
+        value={sensorAttr[1]}
+        onChange={setSensorX}
+      />
+      <label htmlFor="sensorY">y: </label>
+      <input
+        name="sensorY"
+        type="number"
+        min={0}
+        max={canvasHeight}
+        value={sensorAttr[2]}
+        onChange={setSensorY}
+      />
+      <label htmlFor="sensorR">r: </label>
+      <input
+        name="sensorR"
+        type="number"
+        min={0}
+        max={canvasHeight}
+        value={sensorAttr[3]}
+        onChange={setSensorR}
+      />
+      <input
+        type="button"
+        value="add"
+        disabled={!isValidAttr}
+        onClick={addSensor}
+      />
+    </p>
+  </section>
+);
